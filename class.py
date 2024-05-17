ab38057266c63974f0e66c9c65c074da6e976130
DrakBoul <91000709+DrakBoul@users.noreply.github.com>
2024-05-17 09:05:15 -0600

    Create class.py
    Added basic outline of equipment class.

---
+++ class.py
@@ -0,0 +1,9 @@
+class Equipment():
+    def __init__(self, ftn, href, component, ultraSonicClass, TEVInterpretation, classCertainty):
+        self.ftn = ftn
+        self.href = href
+        self.component = component
+        self.ultraSonicClass = ultraSonicClass
+        self.TEVInterpretation = TEVInterpretation
+        self.classCertainty = classCertainty
+        
